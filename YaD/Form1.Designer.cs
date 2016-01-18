91a09ed3c0d235b0a1e3db1ff8e3a9d021cd078c
KasyanenkoID <lazy-777@yandex.ru>
2016-01-18 15:19:06 +0500

    no message

---
+++ YaD/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace YaD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(393, 420);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Папка ТТ";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 200;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Название ТТ";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Онлайн";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 50;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 421);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ YaD/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YaD
+{
+    public partial class Form1 : Form
+    {
+        TT[] arrayTT = new TT[20];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    if (i + 1 < 9)
+            //    {
+            //        arrayTT[i] = new TT(@"C:\YandexDisk\Лавка\TT_0" + i,"");
+            //    }
+            //    else
+            //    {
+            //        arrayTT[i] = new TT(@"C:\YandexDisk\Лавка\TT_" + i, "");
+            //    }
+            //}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            arrayTT[0] = new TT("C:\\YandexDisk\\Лавка\\TT_04", "Порт");
+            arrayTT[1] =new TT("C:\\YandexDisk\\Лавка\\TT_05", "Точка 05");
+            arrayTT[2] =new TT("C:\\YandexDisk\\Лавка\\TT_17", "Точка 17");
+
+            for (int i = 0; i < 3; i++)
+            {
+                dataGridView1.Rows.Add(arrayTT[i].NameTT, arrayTT[i].DirTT);
+                if (File.Exists(arrayTT[0].DirTT + "\\FL*.ttxt"))
+                {
+                    dataGridView1.Rows[i].Cells[2].Style.BackColor = Color.Red;
+                }
+                else
+                {
+                    dataGridView1.Rows[i].Cells[2].Style.BackColor = System.Drawing.Color.Green;
+                }
+            }
+        }
+    }
+
+    class TT
+    {
+        public string NameTT { get; set; }
+        public string DirTT { get; set; }
+
+        public TT()
+        {
+            NameTT = "";
+            DirTT = "";
+        }
+
+        public TT(string NewNameTT, string NewDirTT)
+        {
+            NameTT = NewNameTT;
+            DirTT = NewDirTT;
+        }
+        
+    }
+}
